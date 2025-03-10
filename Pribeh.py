2c433dc0a8e5184550f4c99a64f9313b91234833
MichalNguyen233 <tuan.nguyen024@skolavdf.cz>
2025-03-10 15:38:34 +0100

    Add files via upload

---
+++ Pribeh.py
@@ -0,0 +1,13 @@
+
+color = input("What's your favorite color? ")
+
+number = int(input("What's your favorite number? "))
+
+food = input("What's your favorite food? ")
+
+story = f"Once upon a time, there was a magical {color} dragon who loved eating {food}. "\
+        f"Every day, the dragon would fly over {number} mountains in search of more {food}. "\
+        f"One day, it found a giant {food}-filled cave hidden beneath the tallest mountain! "\
+        f"The dragon was so excited that it invited all its friends for a feast!"
+
+print(story)
